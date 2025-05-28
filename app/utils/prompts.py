f05a5df3a567d72b8704c57e18f8de4053b9464c
Matheus Lacerda <matheus.l1996@gmail.com>
2025-05-28 18:35:49 -0300

    modified generation prompt. focused on better task explaining, and language

--- app/utils/prompts.py
+++ app/utils/prompts.py
@@ -74,11 +74,12 @@ def get_verification_prompt(alert_prompt: str, document: str):
     )
 
 generation_prompt = """
-You are a helpful assistant that generates a payload for an http POST request
+You are a helpful assistant that generate an information alert in a structured format.
 
-The user had set an alert request to be informed when and if the event did happen.
-We received a document that matches the alert's request and the event.
-The payload is to inform the user as he wanted to.
+The user had set an alert request to be informed when and if an event happens.
+We received a document that matches the alert's request and the event did happen.
+You will be given the alert request and the document that matches the alert's request.
+Generate an information alert in a structured format.
 
 
 The alert request was:
@@ -96,10 +97,13 @@ Your response must follow the user's requested payload format exactly:
 {payload_format}
 </payload_format>
 
-You answer must be self-contained, using the document as the source of truth and respond fully to the Query.
+
+You answer must be self-contained, using the document as the source of truth and respond fully to the alert request.
 Your answer must written with an unbiased and journalistic tone.
 You must be succinct. Skip the preamble and just provide the answer without telling the user what you are doing.
-Write in the language of the user's request.
+
+Write in the language/dialect of the user's request.
+If the alert request is in English, your response must be in English. If the alert request is in Spanish, your response must be in Spanish. And so on.
 
 Current date and time: {current_date_time}
 """
@@ -112,28 +116,3 @@ def get_generation_prompt(document: str, payload_format: str, alert_prompt: str)
         alert_prompt=alert_prompt,
         current_date_time=current_date_time
     )
-
-summarization_prompt = """
-You are a helpful assistant that summarizes a document.
-
-
-The document is:
-<document>
-{document}
-</document>
-
-
-Your job is to simply remove whatever is not relevant to the main content of the document.
-Simply remove the non-relevant parts and what is not important to the main content.
-
-
-Current date and time: {current_date_time}
-"""
-
-def get_summarization_prompt(document: str):
-    current_date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return summarization_prompt.format(
-        document=document,
-        current_date_time=current_date_time
-    )
-

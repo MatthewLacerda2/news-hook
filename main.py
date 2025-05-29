5e6d4afb0ab01575690a57e1e974fdced3c90642
Matheus Lacerda <matheus.l1996@gmail.com>
2025-05-29 02:52:11 -0300

    process_document_http

---
+++ main.py
@@ -0,0 +1,14 @@
+import functions_framework
+from app.tasks.manual_document import handle_manual_document
+import asyncio
+
+@functions_framework.http
+def process_document_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        JSON response with status or error
+    """
+    # Cloud Functions need to wrap async code in a sync function
+    return asyncio.run(handle_manual_document(request))

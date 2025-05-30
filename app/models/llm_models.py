4d77f283c2a65f0827c535d156fba2ecda17dfa4
Matheus Lacerda <matheus.l1996@gmail.com>
2025-05-30 01:53:58 -0300

    llm_model model_name primary_key

--- app/models/llm_models.py
+++ app/models/llm_models.py
@@ -4,7 +4,7 @@ from app.models.base import Base
 class LLMModel(Base):
     __tablename__ = "llm_models"
     
-    model_name = Column(String, nullable=False, unique=True)
+    model_name = Column(String, primary_key=True)
     input_token_price = Column(Float, nullable=False)
     output_token_price = Column(Float, nullable=False)
     is_active = Column(Boolean, nullable=False, default=True)

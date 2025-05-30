178e9d7e8e19e21179b71864b2dbf2d5fe654fa9
Matheus Lacerda <matheus.l1996@gmail.com>
2025-05-30 01:17:39 -0300

    nothing to see here

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "News Hook"
     
-    SECRET_KEY: str = "your-secret-key-here-or-youre-gay"  # TODO: Change this in production
+    SECRET_KEY: str = "your-secret-key-here"  # TODO: Change this in production
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 30
     
     GOOGLE_CLIENT_ID: str

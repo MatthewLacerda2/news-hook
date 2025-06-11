ac452d75ac2a3f9bb632b707c3b3a74e41ce0e32
Matheus Lacerda <matheus.l1996@gmail.com>
2025-06-10 22:33:56 -0300

    gitiginore terraform, 10 credits instead of 5000, extra ignore

--- app/core/config.py
+++ app/core/config.py
@@ -24,7 +24,8 @@ class Settings(BaseSettings):
     model_config = ConfigDict(
         case_sensitive=True, 
         env_file=".env",
-        env_file_encoding='utf-8'
+        env_file_encoding='utf-8',
+        extra="ignore"
     )
 
 @lru_cache()

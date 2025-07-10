6d919fedd5ea3b2fb6ce67cef37f2c322a744aba
Matheus Lacerda <matheus.l1996@gmail.com>
2025-07-09 23:33:38 -0300

    initial setup

---
+++ terraform/main.tf
@@ -0,0 +1,71 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_cloudbuild_trigger" "news_hook" {
+  name        = "news-hook-tf-trigger"
+  description = "Builds and deploys news-hook-tf from GitHub main branch"
+
+  github {
+    owner = "MatthewLacerda2"
+    name  = "news-hook"
+    push {
+      branch = "^main$"
+    }
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", "gcr.io/${var.project_id}/${var.service_name}:latest", "."]
+    }
+    images = ["gcr.io/${var.project_id}/${var.service_name}:latest"]
+  }
+}
+
+resource "google_cloud_run_service" "news_hook" {
+  name     = var.service_name
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.project_id}/${var.service_name}:latest"
+        env {
+          name  = "DATABASE_URL"
+          value = var.DATABASE_URL
+        }
+        env {
+          name  = "GOOGLE_REDIRECT_URI"
+          value = var.GOOGLE_REDIRECT_URI
+        }
+        env {
+          name  = "GOOGLE_REFRESH_TOKEN"
+          value = var.GOOGLE_REFRESH_TOKEN
+        }
+        env {
+          name  = "SECRET_KEY"
+          value = var.SECRET_KEY
+        }
+        env {
+          name  = "TELEGRAM_BOT_TOKEN"
+          value = var.TELEGRAM_BOT_TOKEN
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "noauth" {
+  location        = google_cloud_run_service.news_hook.location
+  project         = var.project_id
+  service         = google_cloud_run_service.news_hook.name
+  role            = "roles/run.invoker"
+  member          = "allUsers"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {}
+variable "region" {}
+variable "service_name" {}
+
+variable "DATABASE_URL" {}
+variable "GOOGLE_REDIRECT_URI" {}
+variable "GOOGLE_REFRESH_TOKEN" {}
+variable "SECRET_KEY" {}
+variable "TELEGRAM_BOT_TOKEN" {}
